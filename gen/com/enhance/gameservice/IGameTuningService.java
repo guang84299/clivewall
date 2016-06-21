748aa4fc78ed1e3ae6eb53873bd0d99561671d5b
allen <18608427321@163.com>
2016-06-21 11:14:40 +0800

    no message

--- gen/com/enhance/gameservice/IGameTuningService.java
+++ gen/com/enhance/gameservice/IGameTuningService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/guang/Documents/work/cocos-proj/clivewall/proj.android/src/com/enhance/gameservice/IGameTuningService.aidl
+ * Original file: D:\\clivewall\\proj.android\\src\\com\\enhance\\gameservice\\IGameTuningService.aidl
  */
 package com.enhance.gameservice;
 public interface IGameTuningService extends android.os.IInterface
