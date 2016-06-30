e59efa070f502e9c50d2018fa31c213498fd5d01
严春光 <842997290@qq.com>
2016-06-30 16:14:40 +0800

    更新sdk1.1

--- gen/com/qinglu/livewall/R.java
+++ gen/com/qinglu/livewall/R.java
@@ -20,9 +20,15 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f070000;
+        public static final int imageView=0x7f070002;
+        public static final int layout=0x7f070001;
+        public static final int message=0x7f070004;
+        public static final int title=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int qew_notification=0x7f030001;
+        public static final int qew_notification_pic=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f060001;
